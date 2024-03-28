40ed2552c74434e198bb9f5ff22875f44d32c489
Mohammad Ajaruddin Shaik <shaikazaruddin9951@gmail.com>
2024-03-28 17:19:43 +0000

    solution

---
+++ src/main/java/com/example/wordlyweek/repository/MagazineRepository.java
@@ -0,0 +1,21 @@
+package com.example.wordlyweek.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.wordlyweek.model.Writer;
+import com.example.wordlyweek.model.Magazine;
+
+public interface MagazineRepository {
+    ArrayList<Magazine> getMagazines();
+
+    Magazine getMagazineById(int magazineId);
+
+    Magazine addMagazine(Magazine magazine);
+
+    Magazine updateMagazine(int magazineId, Magazine magazine);
+
+    void deleteMagazine(int magazineId);
+
+    List<Writer> getMagazineWriters(int magazineId);
+}
---
+++ src/main/java/com/example/wordlyweek/repository/WriterRepository.java
@@ -0,0 +1,21 @@
+package com.example.wordlyweek.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.wordlyweek.model.Writer;
+import com.example.wordlyweek.model.Magazine;
+
+public interface WriterRepository {
+    ArrayList<Writer> getWriters();
+
+    Writer getWriterById(int writerId);
+
+    Writer addWriter(Writer writer);
+
+    Writer updateWriter(int writerId, Writer writer);
+
+    void deleteWriter(int writerId);
+
+    List<Magazine> getWriterMagazines(int writerId);
+}
